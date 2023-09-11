65453ea2201659010d0d6d958afe5da2dc8add29
Barabas Raffai <barabas.raffai@gmail.com>
2023-09-11 16:49:49 +0100

    Start set up riscof

--- tools/compliance/crush/env/model_test.h
+++ tools/compliance/crush/env/model_test.h
@@ -1,60 +1,38 @@
-#ifndef _COMPLIANCE_MODEL_H
-#define _COMPLIANCE_MODEL_H
-#define RVMODEL_DATA_SECTION \
-        .pushsection .tohost,"aw",@progbits;                            \
-        .align 8; .global tohost; tohost: .dword 0;                     \
-        .align 8; .global fromhost; fromhost: .dword 0;                 \
-        .popsection;                                                    \
-        .align 8; .global begin_regstate; begin_regstate:               \
-        .word 128;                                                      \
-        .align 8; .global end_regstate; end_regstate:                   \
-        .word 4;
+#ifndef CRUSH_ENV_MODEL_TEST_H
+#define CRUSH_ENV_MODEL_TEST_H
 
-//RV_COMPLIANCE_HALT
-#define RVMODEL_HALT                                              \
-  li x1, 1;                                                                   \
-  write_tohost:                                                               \
-    sw x1, tohost, t5;                                                        \
-    j write_tohost;
+// https://github.com/riscv-non-isa/riscv-arch-test/blob/main/spec/TestFormatSpec.adoc#432-required-model-defined-macros
 
-#define RVMODEL_BOOT
+/* This macro marks the start of signature regions. The test-target should use
+ * this macro to create a label to indicate the begining of the signature
+ * region. For example : .globl begin_signature; begin_signature. This macro
+ * must also begin at a 16-byte boundary and must not include anything else.
+ */
+#define RVMODEL_DATA_BEGIN \
+    .align 4; \
+    .globl begin_signature; begin_signature
 
-//RV_COMPLIANCE_DATA_BEGIN
-#define RVMODEL_DATA_BEGIN                                              \
-  RVMODEL_DATA_SECTION                                                        \
-  .align 4;\
-  .global begin_signature; begin_signature:
-
-//RV_COMPLIANCE_DATA_END
-#define RVMODEL_DATA_END                                                      \
-  .align 4;\
-  .global end_signature; end_signature:  
-
-//RVTEST_IO_INIT
-#define RVMODEL_IO_INIT
-//RVTEST_IO_WRITE_STR
-#define RVMODEL_IO_WRITE_STR(_R, _STR)
-//RVTEST_IO_CHECK
-#define RVMODEL_IO_CHECK()
-//RVTEST_IO_ASSERT_GPR_EQ
-#define RVMODEL_IO_ASSERT_GPR_EQ(_S, _R, _I)
-//RVTEST_IO_ASSERT_SFPR_EQ
-#define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
-//RVTEST_IO_ASSERT_DFPR_EQ
-#define RVMODEL_IO_ASSERT_DFPR_EQ(_D, _R, _I)
-
-#define RVMODEL_SET_MSW_INT       \
- li t1, 1;                         \
- li t2, 0x2000000;                 \
- sw t1, 0(t2);
-
-#define RVMODEL_CLEAR_MSW_INT     \
- li t2, 0x2000000;                 \
- sw x0, 0(t2);
-
-#define RVMODEL_CLEAR_MTIMER_INT
-
-#define RVMODEL_CLEAR_MEXT_INT
+/* This macros marks the end of the signature-region. The test-target must
+ * declare any labels required to indicate the end of the signature region. For
+ * example : .globl end_signature; end_signature. This label must be at a
+ * 16-byte boundary. The entire signature region must be included within the
+ * RVMODEL_DATA_BEGIN macro and the start of the RVMODEL_DATA_END macro. The
+ * RVMODEL_DATA_END macro can also contain other target specific data regions
+ * and initializations but only after the end of the signature.
+ */
+#define RVMODEL_DATA_END \
+    .align 4; \
+    .globl end_signature; end_signature
 
 
-#endif // _COMPLIANCE_MODEL_H
+/* This macros must define the test-target halt mechanism. This macro is
+ * called when the test is to be terminated either due to completion or dur to
+ * unsupported behavior. This macro could also include routines to dump the
+ * signature region to a file on the host system which can be used for
+ * comparison.
+ */
+#define RVMODEL_HALT \
+    la a0, 0x3000_0000 \
+    lw a0, 0(a0)
+
+#endif // CRUSH_ENV_MODEL_TEST_H
