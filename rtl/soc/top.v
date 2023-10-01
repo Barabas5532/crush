669c132073c98d42a9a93953d2a98600229f8d07
Barabas Raffai <barabas.raffai@gmail.com>
2023-10-01 11:14:33 +0100

    WIP set up top level for synthesis

---
+++ rtl/soc/top.v
@@ -0,0 +1,50 @@
+`default_nettype none
+
+module crush #(
+) (
+    input wire clk_i,
+    input wire rst_i
+);
+
+wire stb_o;
+wire cyc_o;
+wire[31:0] adr_o;
+wire[3:0] sel_o;
+wire[31:0] dat_i;
+wire[31:0] dat_o;
+wire we_o;
+wor ack_i;
+wor err_i = 0;
+wor rty_i = 0;
+
+cpu #(.INITIAL_PC('h1000_0000)) dut (
+    .clk_i(clk),
+    .dat_i(dat_i),
+    .dat_o(dat_o),
+    .rst_i(reset),
+    .ack_i(ack_i),
+    .err_i(err_i),
+    .rty_i(rty_i),
+    .stb_o(stb_o),
+    .cyc_o(cyc_o),
+    .adr_o(adr_o),
+    .sel_o(sel_o),
+    .we_o(we_o)
+);
+
+memory #(.BASE_ADDRESS('h2000_0000), .SIZE('h4000)) memory (
+    .clk_i(clk),
+    .rst_i(reset),
+    .stb_i(stb_o),
+    .cyc_i(cyc_o),
+    .adr_i(adr_o),
+    .sel_i(sel_o),
+    .dat_i(dat_o),
+    .dat_o(dat_i),
+    .we_i(we_o),
+    .ack_o(ack_i),
+    .err_o(err_i),
+    .rty_o(rty_i)
+);
+
+endmodule
