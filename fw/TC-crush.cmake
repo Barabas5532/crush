612168838017541fdd1d13295a82dd09dd8c9732
Barabas Raffai <barabas.raffai@gmail.com>
2023-10-28 11:08:33 +0100

    Fix CI

--- fw/TC-crush.cmake
+++ fw/TC-crush.cmake
@@ -1,14 +1,17 @@
 # https://www.vinnie.work/blog/2020-11-17-cmake-eval
 
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_C_COMPILER riscv64-elf-gcc)
 
-# TODO maybe we want startfiles? just nostdlib
+find_program(COMPILER_PATH
+    NAMES
+    # ubuntu and debian
+    riscv32-unknown-elf-gcc
+    riscv64-unknown-elf-gcc
+    # arch linux
+    riscv64-elf-gcc
+    REQUIRED)
+get_filename_component(COMPILER_NAME ${COMPILER_PATH} NAME)
+set(CMAKE_C_COMPILER ${COMPILER_NAME})
+
 set(CMAKE_C_FLAGS "-nostartfiles -nostdlib -march=rv32i -mabi=ilp32")
 set(CMAKE_ASM_FLAGS "-march=rv32i -mabi=ilp32")
-
-# Disable standard library
-# set(CMAKE_C_STANDARD_LIBRARIES "")
-
-# set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
