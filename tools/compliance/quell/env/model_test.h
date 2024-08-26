abfed88218a59b451b631287f28cde763ae3ba64
Barabas Raffai <barabas.raffai@gmail.com>
2024-08-26 15:51:57 +0100

    Set up riscof plugin for quell configuration

---
+++ tools/compliance/quell/env/model_test.h
@@ -0,0 +1,74 @@
+#ifndef CRUSH_ENV_MODEL_TEST_H
+#define CRUSH_ENV_MODEL_TEST_H
+
+// https://github.com/riscv-non-isa/riscv-arch-test/blob/main/spec/TestFormatSpec.adoc#432-required-model-defined-macros
+
+/* This macro marks the start of signature regions. The test-target should use
+ * this macro to create a label to indicate the begining of the signature
+ * region. For example : .globl begin_signature; begin_signature. This macro
+ * must also begin at a 16-byte boundary and must not include anything else.
+ */
+#define RVMODEL_DATA_BEGIN \
+    .align 4; \
+    .globl begin_signature
+
+/* This macros marks the end of the signature-region. The test-target must
+ * declare any labels required to indicate the end of the signature region. For
+ * example : .globl end_signature; end_signature. This label must be at a
+ * 16-byte boundary. The entire signature region must be included within the
+ * RVMODEL_DATA_BEGIN macro and the start of the RVMODEL_DATA_END macro. The
+ * RVMODEL_DATA_END macro can also contain other target specific data regions
+ * and initializations but only after the end of the signature.
+ */
+#define RVMODEL_DATA_END \
+    .align 4; \
+    .globl end_signature
+
+
+/* This macros must define the test-target halt mechanism. This macro is
+ * called when the test is to be terminated either due to completion or dur to
+ * unsupported behavior. This macro could also include routines to dump the
+ * signature region to a file on the host system which can be used for
+ * comparison.
+ */
+#define RVMODEL_HALT \
+    la x1, rvtest_sig_begin; \
+    la x2, 0x30000000; \
+    sw x1, 0(x2); \
+    la x1, rvtest_sig_end; \
+    sw x1, 4(x2); \
+    lw x1, 8(x2)
+
+/* contains boot code for the test-target; may include emulation code or trap
+ * stub. If the test-target enforces alignment or value restrictions on the
+ * mtvec csr, it is required that this macro sets the value of mtvec to a
+ * region which is readable and writable by the machine mode. May include code
+ * to copy the data sections from boot device to ram. Or any other code that
+ * needs to be run prior to running the tests.
+ */
+#define RVMODEL_BOOT \
+    /* Copy data section from flash to RAM */ \
+    la x1, _srelocate; \
+    la x2, _erelocate; \
+    la x3, _etext; \
+rvmodel_boot_loop: \
+    beq x1, x2, rvmodel_boot_done; \
+    lw x31, 0(x3); \
+    sw x31, 0(x1); \
+    addi x1, x1, 4; \
+    addi x3, x3, 4; \
+    j rvmodel_boot_loop; \
+rvmodel_boot_done:
+
+/* debug assertion that GPR should have value
+ *
+ * outputs a debug message if Reg!=Value
+ *
+ * ScrReg is a scratch register used by the output routine; its final value
+ * cannot be guaranteed
+ *
+ * Can be used to help debug what tests have passed/failed
+ */
+#define RVMODEL_IO_ASSERT_GPR_EQ(ScrReg, Reg, Value)
+
+#endif // CRUSH_ENV_MODEL_TEST_H
---
+++ tools/compliance/quell/riscof_quell.py
@@ -0,0 +1,93 @@
+import os
+import re
+import shutil
+import subprocess
+import shlex
+import logging
+import random
+import string
+from string import Template
+import sys
+import yaml
+from multiprocessing import Pool
+
+import riscof.utils as utils
+import riscof.constants as constants
+from riscof.pluginTemplate import pluginTemplate
+
+logger = logging.getLogger()
+
+class quell(pluginTemplate):
+    __model__ = "quell"
+    __version__ = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        config = kwargs['config']
+        self.compiler_name_prefix = config['compiler_name_prefix']
+
+        with open('../../crush.core') as f:
+            core = yaml.load(f, Loader=yaml.Loader)
+
+        core_name = core['name']
+        logger.info(f'Read fusesoc name: {core_name}')
+        version = core_name.split(':')[-1]
+
+        cwd = os.getcwd()
+        logger.info(f'Current working directory: {cwd}')
+
+        self.dut_exe = os.path.join(cwd, f'../../build/crush_{version}/sim_cpu-icarus/crush_{version}')
+
+        self.isa_spec = os.path.join(cwd, 'quell/quell_isa.yaml')
+        self.platform_spec = os.path.join(cwd, 'quell/quell_platform.yaml')
+
+    def initialise(self, suite, work_dir, archtest_env):
+        self.working_directory = work_dir
+        self.archtest_env = archtest_env
+
+    def build(self, isa_yaml, platform_yaml):
+        build_cmd = 'fusesoc run --target sim_cpu --build --resolve-env-vars-early crush'
+        logger.info(f'Build simulator: {build_cmd}')
+        utils.shellCommand(build_cmd).run(cwd=os.path.join(os.getcwd(), '../..'))
+
+    def runTests(self, test_list):
+        logger.info(f'Running {len(test_list)} tests in parallel')
+
+        with Pool(len(test_list)) as p:
+            p.map(self.doRunTest, [test_list[x] for x in test_list])
+
+    def doRunTest(self, test_entry):
+        test_source_path = test_entry['test_path']
+        working_directory = test_entry['work_dir']
+
+        # name of the signature file as per requirement of RISCOF. RISCOF
+        # expects the signature to be named as DUT-<dut-name>.signature. The
+        # below variable creates an absolute path of signature file.
+        sig_file = os.path.join(working_directory, self.name[:-1] + ".signature")
+
+        compile_macros = ' -D' + " -D".join(test_entry['macros'])
+        cmd = (f'{self.compiler_name_prefix}-gcc '
+                '-march=rv32i '
+                '-mabi=ilp32 '
+                '-static '
+                '-mcmodel=medany '
+                '-fvisibility=hidden '
+                '-nostdlib '
+                '-nostartfiles '
+                '-g '
+                f'-T {os.path.join(os.getcwd(), "quell/env/link.ld")} '
+                f'-I {os.path.join(os.getcwd(), "quell/env/")} '
+                f'-I {self.archtest_env} '
+                f'{compile_macros} '
+                '-o test.elf ' +
+                test_source_path
+              )
+        utils.shellCommand(cmd).run(cwd=working_directory)
+
+        binary_path = os.path.join(working_directory, 'test.bin')
+        binary_cmd = f'{self.compiler_name_prefix}-objcopy -O binary test.elf {binary_path}'
+        utils.shellCommand(binary_cmd).run(cwd=working_directory)
+
+        sim_cmd = f'vvp -n {self.dut_exe} +SIGNATURE_PATH={sig_file} +BINARY_PATH={binary_path}'
+        utils.shellCommand(sim_cmd).run(cwd=working_directory)
