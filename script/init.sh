a4f033d5633d1e25bccdb85db60f0a9b4195826c
Barabas Raffai <barabas.raffai@gmail.com>
2024-12-09 14:44:35 +0000

    fix fusesoc setup

--- script/init.sh
+++ script/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 export YOSYS_DATDIR=$(yosys-config --datdir)
+
+rm -rf fusesoc_libraries
 fusesoc library add --sync-type local crush .
+fusesoc library add fusesoc_cores https://github.com/fusesoc/fusesoc-cores
