83de9af4ee8798765c8f3408846623267acedd3e
Barabas Raffai <barabas.raffai@gmail.com>
2025-01-23 15:09:01 +0000

    fix some tests

--- script/init.sh
+++ script/init.sh
@@ -3,3 +3,4 @@
 export YOSYS_DATDIR=$(yosys-config --datdir)
 fusesoc library add --sync-type local crush crush
 fusesoc library add --sync-type local crush_example crush_example
+fusesoc library add --sync-type local crush_support support
