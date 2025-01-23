3485ccd0b42adadebc1c59ee5159c249c16ea426
Barabas Raffai <barabas.raffai@gmail.com>
2025-01-23 14:32:55 +0000

    Refactor file structure

--- script/init.sh
+++ script/init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export YOSYS_DATDIR=$(yosys-config --datdir)
-fusesoc library add --sync-type local crush .
+fusesoc library add --sync-type local crush crush
+fusesoc library add --sync-type local crush_example crush_example
