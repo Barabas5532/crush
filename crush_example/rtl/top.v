682c733637a5592f2dd6db5690eded3537f0336f
Barabas Raffai <barabas.raffai@gmail.com>
2025-04-07 16:41:51 +0100

    WIP fix synthesis

--- crush_example/rtl/top.v
+++ crush_example/rtl/top.v
@@ -11,77 +11,124 @@ module top (
 wire rst_i = !BTN_N;
 wire clk = CLK;
 
-wire stb_o;
-wire cyc_o;
-wire[31:0] adr_o;
-wire[3:0] sel_o;
-wire[31:0] dat_i;
-wire[31:0] dat_o;
-wire we_o;
-wire ack_i;
-wire err_i;
-wire rty_i;
+wire        wb_inst_stb_o;
+wire        wb_inst_cyc_o;
+wire[31:0]  wb_inst_adr_o;
+wire[3:0]   wb_inst_sel_o;
+wire[31:0]  wb_inst_dat_i;
+wire[31:0]  wb_inst_dat_o;
+wire        wb_inst_we_o;
+wire        wb_inst_ack_i;
+wire        wb_inst_err_i;
+wire        wb_inst_rty_i;
 
-crush_cpu #(.INITIAL_PC('h1000_0000)) cpu (
+wire        wb_data_stb_o;
+wire        wb_data_cyc_o;
+wire[31:0]  wb_data_adr_o;
+wire[3:0]   wb_data_sel_o;
+wire[31:0]  wb_data_dat_i;
+wire[31:0]  wb_data_dat_o;
+wire        wb_data_we_o;
+wire        wb_data_ack_i;
+wire        wb_data_err_i;
+wire        wb_data_rty_i;
+
+crush_cpu #(.INITIAL_PC('h0000_0000)) cpu (
     .clk_i(clk),
-    .dat_i(dat_i),
-    .dat_o(dat_o),
     .rst_i(rst_i),
-    .ack_i(ack_i),
-    .err_i(err_i),
-    .rty_i(rty_i),
-    .stb_o(stb_o),
-    .cyc_o(cyc_o),
-    .adr_o(adr_o),
-    .sel_o(sel_o),
-    .we_o(we_o)
+    .wb_inst_dat_i(wb_inst_dat_i),
+    .wb_inst_dat_o(wb_inst_dat_o),
+    .wb_inst_ack_i(wb_inst_ack_i),
+    .wb_inst_err_i(wb_inst_err_i),
+    .wb_inst_rty_i(wb_inst_rty_i),
+    .wb_inst_stb_o(wb_inst_stb_o),
+    .wb_inst_cyc_o(wb_inst_cyc_o),
+    .wb_inst_adr_o(wb_inst_adr_o),
+    .wb_inst_sel_o(wb_inst_sel_o),
+    .wb_inst_we_o(wb_inst_we_o),
+    .wb_data_dat_i(wb_data_dat_i),
+    .wb_data_dat_o(wb_data_dat_o),
+    .wb_data_ack_i(wb_data_ack_i),
+    .wb_data_err_i(wb_data_err_i),
+    .wb_data_rty_i(wb_data_rty_i),
+    .wb_data_stb_o(wb_data_stb_o),
+    .wb_data_cyc_o(wb_data_cyc_o),
+    .wb_data_adr_o(wb_data_adr_o),
+    .wb_data_sel_o(wb_data_sel_o),
+    .wb_data_we_o(wb_data_we_o)
 );
 
-wire memory_ack_o;
-wire memory_rty_o;
-wire memory_err_o;
+wire wb_inst_instruction_memory_ack_o;
+wire wb_inst_instruction_memory_rty_o;
+wire wb_inst_instruction_memory_err_o;
+  memory_infer #(
+      .BASE_ADDRESS('h0000_0000),
+      .SIZE('h400),
+      .READMEMH_FILE("inst.data")
+  ) instruction_memory (
+      .clk_i(clk),
+      .rst_i(rst_i),
+      .stb_i(wb_inst_stb_o),
+      .cyc_i(wb_inst_cyc_o),
+      .adr_i(wb_inst_adr_o),
+      .sel_i(wb_inst_sel_o),
+      .dat_i(wb_inst_dat_o),
+      .dat_o(wb_inst_dat_i),
+      .we_i (wb_inst_we_o),
+      .ack_o(wb_inst_instruction_memory_ack_o),
+      .err_o(wb_inst_instruction_memory_err_o),
+      .rty_o(wb_inst_instruction_memory_rty_o)
+  );
+
+wire wb_data_memory_ack_o;
+wire wb_data_memory_rty_o;
+wire wb_data_memory_err_o;
   memory_infer #(
       .BASE_ADDRESS('h1000_0000),
-      .SIZE('h800),
-      .READMEMH_FILE("fw.data")
+      .SIZE('h400),
+      .READMEMH_FILE("data.data")
   ) memory (
       .clk_i(clk),
       .rst_i(rst_i),
-      .stb_i(stb_o),
-      .cyc_i(cyc_o),
-      .adr_i(adr_o),
-      .sel_i(sel_o),
-      .dat_i(dat_o),
-      .dat_o(dat_i),
-      .we_i (we_o),
-      .ack_o(memory_ack_o),
-      .err_o(memory_err_o),
-      .rty_o(memory_rty_o)
+      .stb_i(wb_data_stb_o),
+      .cyc_i(wb_data_cyc_o),
+      .adr_i(wb_data_adr_o),
+      .sel_i(wb_data_sel_o),
+      .dat_i(wb_data_dat_o),
+      .dat_o(wb_data_dat_i),
+      .we_i (wb_data_we_o),
+      .ack_o(wb_data_memory_ack_o),
+      .err_o(wb_data_memory_err_o),
+      .rty_o(wb_data_memory_rty_o)
   );
 
-wire gpio_ack_o;
-wire gpio_rty_o;
-wire gpio_err_o;
+wire wb_data_gpio_ack_o;
+wire wb_data_gpio_rty_o;
+wire wb_data_gpio_err_o;
 wire [5:0] unused;
 gpio #(.BASE_ADDRESS('h4000_0000)) gpio (
     .clk_i(clk),
     .rst_i(rst_i),
-    .stb_i(stb_o),
-    .cyc_i(cyc_o),
-    .adr_i(adr_o),
-    .sel_i(sel_o),
-    .dat_i(dat_o),
-    .dat_o(dat_i),
-    .we_i(we_o),
-    .ack_o(gpio_ack_o),
-    .err_o(gpio_err_o),
-    .rty_o(gpio_rty_o),
+    .stb_i(wb_data_stb_o),
+    .cyc_i(wb_data_cyc_o),
+    .adr_i(wb_data_adr_o),
+    .sel_i(wb_data_sel_o),
+    .dat_i(wb_data_dat_o),
+    .dat_o(wb_data_dat_i),
+    .we_i(wb_data_we_o),
+    .ack_o(wb_data_gpio_ack_o),
+    .err_o(wb_data_gpio_err_o),
+    .rty_o(wb_data_gpio_rty_o),
     .pin_input({{7{1'b0}}, BTN1}),
     .pin_output({unused, LED2, LED1})
 );
 
-assign ack_i = memory_ack_o | gpio_ack_o;
-assign err_i = memory_err_o | gpio_err_o;
-assign rty_i = memory_rty_o | gpio_rty_o;
+assign wb_data_ack_i = wb_data_memory_ack_o | wb_data_gpio_ack_o;
+assign wb_data_err_i = wb_data_memory_err_o | wb_data_gpio_err_o;
+assign wb_data_rty_i = wb_data_memory_rty_o | wb_data_gpio_rty_o;
+
+assign wb_inst_ack_i = wb_inst_instruction_memory_ack_o;
+assign wb_inst_err_i = wb_inst_instruction_memory_err_o;
+assign wb_inst_rty_i = wb_inst_instruction_memory_rty_o;
 
 endmodule
